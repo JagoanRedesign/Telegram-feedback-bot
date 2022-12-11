d9c6054903d453076d2e30eebd838be4e3441137
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2022-12-11 08:33:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TG_TOKEN = "YOUR TOKEN HERE"
+TG_TOKEN = "5550434735:AAEJ7lcyTs8ApsQs4wmWCczwBzQVC95ieuw"
 PROXY = None
-CHAT_ID = -100125 # The chat group or individual chat id of forwarded message destination
+CHAT_ID = -1001853342702 # The chat group or individual chat id of forwarded message destination
 BOT_TIMEOUT = 10
 BOT_RETRIES = 5
 USER_JSON = 'user_book.json'
